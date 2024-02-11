b3a722e2b5ad4f31124fe0806aaed30ef935317f
Jonathan Santiago <santiagowriter@gmail.com>
2024-02-10 21:54:24 -0500

    create file

--- content/index.md
+++ content/index.md
@@ -5,4 +5,3 @@ title: Welcome to my Gospel Garden
 This is a blank Quartz installation.
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
 
-This is Jonathan!
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Keller Study Bible",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
