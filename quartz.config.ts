2ff11d19b1a843480158618fa8d9309acf75cfb3
Jonathan Santiago <santiagowriter@gmail.com>
2024-02-12 22:42:37 -0500

    EB Garamond font

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "EB Garamond",
         code: "IBM Plex Mono",
       },
       colors: {
