4bd15dbe510f22a8737f42ccc6bef2569069ec20
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-06-22 00:26:09 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jon's Digital Study Bible",
+    pageTitle: "Gospel Exploder",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
