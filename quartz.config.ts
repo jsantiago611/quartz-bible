13d5bd6e605e149375ae2899c265f5b0baa3884a
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-07-06 09:42:41 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gospel Exploder",
+    pageTitle: "Gospel Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
