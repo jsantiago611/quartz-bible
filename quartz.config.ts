8f2fe9acd9c2646923e39b716360085b7b02a7d2
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-09-18 12:46:47 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Keller Study Bible",
+    pageTitle: "Jon's Commonplace Book",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
