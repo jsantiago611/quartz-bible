0392307c44867cbb410e0ad29e5e4518ad7d6c7b
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-09-18 17:02:59 -0400

    updating site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jon's Commonplace Book",
+    pageTitle: "Jonathan's Commonplace Book",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
