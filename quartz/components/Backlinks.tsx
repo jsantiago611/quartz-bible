f3efbf70a7c80fd173d3c76e8fd14f25b5a67d90
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-10-15 13:48:17 -0400

    Trying out red text

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -7,14 +7,21 @@ import { classNames } from "../util/lang"
 function Backlinks({ fileData, allFiles, displayClass, cfg }: QuartzComponentProps) {
   const slug = simplifySlug(fileData.slug!)
   const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+
+  // **NEW**: Check if the fileData has the tag "red"
+  const hasRedTag = fileData.tags?.includes("red");
+
   return (
     <div class={classNames(displayClass, "backlinks")}>
       <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
       <ul class="overflow">
         {backlinkFiles.length > 0 ? (
           backlinkFiles.map((f) => (
-            <li>
-              <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
+            <li key={f.slug}> {/* **NEW**: Added key for each mapped item */}
+              <a
+                href={resolveRelative(fileData.slug!, f.slug!)}
+                class={classNames("internal", { "red-link": hasRedTag })} // **MODIFIED**: Conditionally add 'red-link' class
+              >
                 {f.frontmatter?.title}
               </a>
             </li>
