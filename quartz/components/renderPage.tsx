cb74028764db8f43a522d8436d163fe81a746422
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-10-15 18:31:59 -0400

    Update renderPage.tsx

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -1,3 +1,64 @@
+import { render } from "preact-render-to-string";
+import { QuartzComponent, QuartzComponentProps } from "./types";
+import HeaderConstructor from "./Header";
+import BodyConstructor from "./Body";
+import { JSResourceToScriptElement, StaticResources } from "../util/resources";
+import { FullSlug, RelativeURL, joinSegments, normalizeHastElement } from "../util/path";
+import { visit } from "unist-util-visit";
+import { Root } from "hast";
+import { QuartzPluginData } from "../plugins/vfile";
+import { GlobalConfiguration } from "../cfg";
+import { i18n } from "../i18n";
+interface RenderComponents {
+  head: QuartzComponent;
+  header: QuartzComponent[];
+  beforeBody: QuartzComponent[];
+  pageBody: QuartzComponent;
+  left: QuartzComponent[];
+  right: QuartzComponent[];
+  footer: QuartzComponent;
+}
+export function pageResources(
+  baseDir: FullSlug | RelativeURL,
+  staticResources: StaticResources,
+): StaticResources {
+  const contentIndexPath = joinSegments(baseDir, "static/contentIndex.json");
+  const contentIndexScript = `const fetchData = fetch("${contentIndexPath}").then(data => data.json())`;
+  return {
+    css: [joinSegments(baseDir, "index.css"), ...staticResources.css],
+    js: [
+      {
+        src: joinSegments(baseDir, "prescript.js"),
+        loadTime: "beforeDOMReady",
+        contentType: "external",
+      },
+      {
+        loadTime: "beforeDOMReady",
+        contentType: "inline",
+        spaPreserve: true,
+        script: contentIndexScript,
+      },
+      ...staticResources.js,
+      {
+        src: joinSegments(baseDir, "postscript.js"),
+        loadTime: "afterDOMReady",
+        moduleType: "module",
+        contentType: "external",
+      },
+    ],
+  };
+}
+let pageIndex: Map<FullSlug, QuartzPluginData> | undefined = undefined;
+function getOrComputeFileIndex(allFiles: QuartzPluginData[]): Map<FullSlug, QuartzPluginData> {
+  if (!pageIndex) {
+    pageIndex = new Map();
+    for (const file of allFiles) {
+      pageIndex.set(file.slug!, file);
+    }
+  }
+  return pageIndex;
+}
+
 export function renderPage(
   cfg: GlobalConfiguration,
   slug: FullSlug,
