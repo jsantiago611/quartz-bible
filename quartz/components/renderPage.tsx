2914b73291ac62cdd640934ec537562ed68583a0
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-10-15 18:13:28 -0400

    adds serverside/clientside console messages

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -1,70 +1,3 @@
-import { render } from "preact-render-to-string";
-import { QuartzComponent, QuartzComponentProps } from "./types";
-import HeaderConstructor from "./Header";
-import BodyConstructor from "./Body";
-import { JSResourceToScriptElement, StaticResources } from "../util/resources";
-import { FullSlug, RelativeURL, joinSegments, normalizeHastElement } from "../util/path";
-import { visit } from "unist-util-visit";
-import { Root } from "hast";
-import { QuartzPluginData } from "../plugins/vfile";
-import { GlobalConfiguration } from "../cfg";
-import { i18n } from "../i18n";
-
-interface RenderComponents {
-  head: QuartzComponent;
-  header: QuartzComponent[];
-  beforeBody: QuartzComponent[];
-  pageBody: QuartzComponent;
-  left: QuartzComponent[];
-  right: QuartzComponent[];
-  footer: QuartzComponent;
-}
-
-export function pageResources(
-  baseDir: FullSlug | RelativeURL,
-  staticResources: StaticResources,
-): StaticResources {
-  const contentIndexPath = joinSegments(baseDir, "static/contentIndex.json");
-  const contentIndexScript = `const fetchData = fetch("${contentIndexPath}").then(data => data.json())`;
-
-  return {
-    css: [joinSegments(baseDir, "index.css"), ...staticResources.css],
-    js: [
-      {
-        src: joinSegments(baseDir, "prescript.js"),
-        loadTime: "beforeDOMReady",
-        contentType: "external",
-      },
-      {
-        loadTime: "beforeDOMReady",
-        contentType: "inline",
-        spaPreserve: true,
-        script: contentIndexScript,
-      },
-      ...staticResources.js,
-      {
-        src: joinSegments(baseDir, "postscript.js"),
-        loadTime: "afterDOMReady",
-        moduleType: "module",
-        contentType: "external",
-      },
-    ],
-  };
-}
-
-let pageIndex: Map<FullSlug, QuartzPluginData> | undefined = undefined;
-
-function getOrComputeFileIndex(allFiles: QuartzPluginData[]): Map<FullSlug, QuartzPluginData> {
-  if (!pageIndex) {
-    pageIndex = new Map();
-    for (const file of allFiles) {
-      pageIndex.set(file.slug!, file);
-    }
-  }
-
-  return pageIndex;
-}
-
 export function renderPage(
   cfg: GlobalConfiguration,
   slug: FullSlug,
@@ -72,11 +5,15 @@ export function renderPage(
   components: RenderComponents,
   pageResources: StaticResources,
 ): string {
-  // Log the component data at the start of the function
-  console.log("Component Data:", componentData);
 
-  // Log the frontmatter tags
-  console.log("Frontmatter Object:", componentData.frontmatter);
+  // Check if we're in a browser or server environment
+  if (typeof window !== "undefined") {
+    // This will log in the browser (client-side)
+    console.log("Client-side Frontmatter Object:", componentData.frontmatter);
+  } else {
+    // This will log on the server (server-side)
+    console.log("Server-side Frontmatter Object:", componentData.frontmatter);
+  }
 
   // Check for the presence of the 'red' tag
   const hasRedTag = componentData.frontmatter?.tags?.includes("red");
@@ -96,6 +33,7 @@ export function renderPage(
     right,
     footer: Footer,
   } = components;
+  
   const Header = HeaderConstructor();
   const Body = BodyConstructor();
 
