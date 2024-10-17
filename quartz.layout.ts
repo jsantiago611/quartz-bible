9ae1fd988e1e6a7ab15d3d330837179eadb81665
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-10-17 15:17:46 -0400

    Removing TOC from the right sidebar

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    // Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
