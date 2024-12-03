10f51f9c5ddc3ca3135c47e5a71644e3f1242abe
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-12-03 21:07:35 +0000

    minor UI adjustments like borders andadding graph back in

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Atomic Apologetics",
+    pageTitle: "The Apologetics Atlas",
     enableSPA: true,
     enablePopovers: true,
     generateSocialImages: true,
