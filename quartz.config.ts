e576d9312172ebce5ad0b0cf8e33d82dbfba8e24
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-04-14 17:29:08 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Marist",
-        body: "Marist",
+        body: "SharpRoman",
         code: "droulers",
       },
       colors: {
